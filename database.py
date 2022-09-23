e89029b1ab4dd1e96114a32b7c131935999b9bbc
KrzysKolo <krzysztofa.kolodziejczak@gmail.com>
2022-09-23 13:56:34 +0200

    first commit

---
+++ database.py
@@ -0,0 +1,12 @@
+import harperdb
+from secrets import HARPERDB_URL, HARPERDB_USERNAME, HARPERDB_PASSWORD
+HARPERDB_URL = '***************',
+HARPERDB_USERNAME = '**********',
+HARPERDB_PASSWORD = '**********'
+
+
+db = harperdb.HarperDB(
+    url=HARPERDB_URL,
+    username=HARPERDB_USERNAME,
+    password=HARPERDB_PASSWORD,
+)
---
+++ secrets.py
@@ -0,0 +1,4 @@
+import harperdb
+HARPERDB_URL = 'https://krzys-kkolodziejczak.harperdbcloud.com',
+HARPERDB_USERNAME = 'krzys',
+HARPERDB_PASSWORD = 'kolo12345$$'
